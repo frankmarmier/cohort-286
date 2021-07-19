43d0e60e7bb3f8d8229907e827896a17f3ade267
1testsample <frankomarmier@gmail.com>
2021-07-19 10:27:05 +0200

    Module 2 intro

---
+++ module-2/week-1/day-1/express-intro/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+// app.get("/",function(){
+
+// })
+
+app.use(express.static(__dirname + "/public"));
+
+console.log("Directory name", __dirname);
+
+app.get("/", (request, response, next) => {
+  //   response.send("<h1>Hello my friends !</h1>");
+  //   response.sendFile(__dirname + "/html/home.html");
+  response.render("home.hbs");
+});
+
+app.get("/about", (req, res, next) => {
+  //   res.sendFile(__dirname + "/html/about.html");
+  res.render("about.hbs");
+});
+
+app.get("/contact", (req, res, next) => {
+  //   res.sendFile(__dirname + "/html/contact.html");
+  res.render("contact.hbs");
+});
+
+// app.get("/styles/index.css", (req, res, next) => {
+//   res.sendFile(__dirname + "/styles/index.css");
+// });
+
+app.listen(4000);
