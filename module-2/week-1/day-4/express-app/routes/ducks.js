3b6174ca290bfc45052d7702af3e790579daef34
1testsample <frankomarmier@gmail.com>
2021-07-22 11:23:31 +0200

    Add params and query

--- module-2/week-1/day-4/express-app/routes/ducks.js
+++ module-2/week-1/day-4/express-app/routes/ducks.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const RubberDuck = require("../models/RubberDuck");
+const mongoose = require("mongoose");
+
 const router = express.Router();
 
 router.get("/ducks", (req, res) => {
-  RubberDuck.find()
+  console.log(req.query);
+  //   res.send(req.query);
+
+  //   console.log(req.query);
+  RubberDuck.find(req.query)
     .then((rubberDuckDocuments) => {
       res.render("ducks/listDuck.hbs", {
         ducks: rubberDuckDocuments,
@@ -13,5 +19,41 @@ router.get("/ducks", (req, res) => {
       console.log(error);
     });
 });
+
+// router.get("/ducks", (req, res) => {
+//   RubberDuck.find()
+//     .then((rubberDuckDocuments) => {
+//       res.render("ducks/listDuck.hbs", {
+//         ducks: rubberDuckDocuments,
+//       });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+
+router.get("/ducks/:id", (req, res) => {
+  const isValid = mongoose.isValidObjectId(req.params.id);
+
+  if (isValid) {
+    RubberDuck.findById(req.params.id)
+      .then((duckDocument) => {
+        console.log(duckDocument);
+        res.render("ducks/oneDuck.hbs", {
+          duck: duckDocument,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    res.render("not-found.hbs");
+  }
+});
+
+// router.get("/ducks/:id", (req, res) => {
+//   console.log(req.params);
+//   res.send(req.params);
+// });
 
 module.exports = router;
