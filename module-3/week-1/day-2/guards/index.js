ee65ae51bb68f826d46afecb065f7a3230871856
1testsample <frankomarmier@gmail.com>
2021-08-10 12:27:57 +0200

    Add guards

---
+++ module-3/week-1/day-2/guards/index.js
@@ -0,0 +1,13 @@
+// // true && true
+// true && true && true && true && true;
+
+// false && true;
+
+// console.log(true && "yo");
+// console.log(true && "yo" && true);
+// console.log(true && "yo" && true && "anotherone");
+// console.log(false && "yo" && true && "anotherone");
+
+// console.log("yo" || true);
+// console.log(true || "yo");
+// console.log(false || "yo" || true);
