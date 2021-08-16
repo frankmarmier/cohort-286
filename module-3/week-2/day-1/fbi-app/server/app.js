bea7d462c165db25a0b00d5d74db7d38930fae2b
1testsample <frankomarmier@gmail.com>
2021-08-16 13:23:14 +0200

    Add day1

--- module-3/week-2/day-1/fbi-app/server/app.js
+++ module-3/week-2/day-1/fbi-app/server/app.js
@@ -3,14 +3,16 @@ require("dotenv/config");
 require("./config/dbConnection");
 
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const cors = require("cors");
 const app = express();
+
+/**
+ * Middlewares
+ */
+//  https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+// This middleware is required to authorize a client browser application hosted on a different domain
 
 app.use(
   cors({
@@ -20,19 +22,24 @@ app.use(
     //   "http://localhost:3001",
     //   "www.facebook.com",
     // ], // allow several domains
-    // origin: "*",  // allow any frontend domain to communicate with this API
+    // origin: "*",  // allow any browser  domain to communicate with this API
   })
 );
 
-app.use(logger("dev"));
+app.use(logger("dev")); // Logs http requests to the console
 app.use(express.json()); // reads application/json headers and exposes data @req.body
 app.use(express.urlencoded({ extended: false })); // reads application/x-www-form-urlencoded and exposes data @req.body
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use(express.static(path.join(__dirname, "public"))); // NOT NEEDED FOR NOW
+
+/**
+ * Routes
+ */
 
 app.use("/api/fugitives", require("./routes/fugitives"));
-app.use("/users", usersRouter);
+
+/**
+ * Error handling middlewares
+ */
 
 module.exports = app;
